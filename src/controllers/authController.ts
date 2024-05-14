fed440f692eed2ee1d0d454c9b55db4dca0d88c8
“Nataly <natalyrojasm5@gmail.com>
2024-05-13 23:20:29 -0500

    Fix: Create Account

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,29 +1,19 @@
 import type { Request, Response } from "express";
-// import { User } from "@prisma/client";
 import { hashPassword } from "../utils/auth";
-import { prisma } from "../data/postgres";
-// import { IUser } from '../models/user';
-// import prisma from "../models/user";
+import prisma from "../lib/prisma";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
-      const { password } = req.body;
-      console.log(password);
+      const { password_confirmation, ...rest } = req.body;
+      const confirmed = await hashPassword(password_confirmation);
       const user = await prisma.user.create({
-        data: { password },
+        data: { ...rest, confirmed },
       });
-      // const user = await prisma.create(req.body);
-
-      // Hash Password
-      user.password = await hashPassword(password);
-      // await user.save();
-
       res.json(user);
-      res.send("Cuenta creada, revisa tu email para confirmarla");
     } catch (error) {
       console.log(error);
-      res.status(500).json({ error: "Hubo un error" });
     }
+
   };
 }
